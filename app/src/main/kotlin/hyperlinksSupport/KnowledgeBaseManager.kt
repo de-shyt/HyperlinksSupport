a068abbc0f5a9455f7b4a6aa65a13c7ed9c23460
de-shyt <de.shut@inbox.ru>
2024-03-29 12:25:59 +0100

    comments for KnowledgeBaseManager

--- app/src/main/kotlin/hyperlinksSupport/KnowledgeBaseManager.kt
+++ app/src/main/kotlin/hyperlinksSupport/KnowledgeBaseManager.kt
@@ -2,18 +2,30 @@ package hyperlinksSupport
 
 import java.io.File
 
+/**
+ * Responsible for applying changes to files of a specific knowledge base
+ */
 class KnowledgeBaseManager {
     private val knowledgeBaseId: String
     private val workingDir: String
-    private var currentFile: File
     private val listOfFiles = HashMap<String, File>()
 
     constructor(knowledgeBaseId: String, initialFile: File) {
         this.knowledgeBaseId = knowledgeBaseId
         this.workingDir = initialFile.parent
-
-        this.currentFile = initialFile
-
         listOfFiles[initialFile.name] = initialFile
+    }
+
+    fun moveToNextFile(fileName: String): File {
+        if (!listOfFiles.containsKey(fileName)) {
+            val file = File(workingDir, fileName)
+            if (!file.exists()) {
+                val created = file.createNewFile()
+                require(created) { "Error while trying to create a file '$fileName' in '$knowledgeBaseId'" }
+            }
+            listOfFiles[fileName] = file
+        }
+
+        return listOfFiles[fileName]!!
     }
 }
